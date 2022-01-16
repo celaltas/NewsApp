011c3c06dabaf7c73469899c3833b3057527a8f2
celal <celal.tas123@gmail.com>
2022-01-16 23:03:03 +0300

    fragments and navigation

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "5def1b9569ac464d8055a50d83cf8c92"
+        const val BASE_URL = "https://newsapi.org"
+    }
+
+}
