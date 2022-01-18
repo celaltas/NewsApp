363c6de3679b67fa489f5000ce15e291e8620e3c
celal <celal.tas123@gmail.com>
2022-01-18 23:33:48 +0300

    Search views and webview created

--- app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
@@ -2,6 +2,7 @@ package com.androiddevs.mvvmnewsapp.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 
 @Entity(
@@ -21,4 +22,4 @@ data class Article(
     val title: String,
     val url: String,
     val urlToImage: String
-)
+):Serializable
--- app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -5,6 +5,7 @@ class Constants {
     companion object{
         const val API_KEY = "5def1b9569ac464d8055a50d83cf8c92"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 
 }
